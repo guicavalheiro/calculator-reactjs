1b2cc781e4abd7f6510af62f3a1d4975aee9e00c
guicavalheiro <cavalheiro.dev@gmail.com>
2021-02-15 10:31:23 -0300

    first commit

---
+++ src/components/Screen.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import './Screen.css'
+
+class Screen extends Component {
+    render() {
+        return (
+            <div className='screen'>
+                teste
+            </div>
+        );
+    }
+}
+
+export default Screen;
